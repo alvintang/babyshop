29f30a7e8ceac2818491af666f041127ceee470d
Alvin Tang <alvin@maroonstudios.com>
2017-10-30 04:44:07 +0800

    fixed credit card flow

--- base/static/js/payment.js
+++ base/static/js/payment.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
     option = $(this).val();
     console.log(option);
 
-
-
     if(option == 1){
       $('.payment-info-bank').show();
       $('.payment-info-paypal').hide();
+      $('#submitForm').show();
     }else if (option == 2){
       $('.payment-info-bank').hide();
       $('.payment-info-paypal').show();
+      $('#submitForm').hide();
     }
   });
 
